b58d8b038b204175c2047a9f48f57db59be5048f
Asif Shakir <asif.shakir@gmail.com>
2023-03-05 09:06:34 +0530

    Add project files.

---
+++ BBK.HRMS.API.Auth/LoginController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BBK.Auth.Areas.Auth
+{
+    [ApiController]
+    [Produces("application/json")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/auth/login")]
+        [ProducesResponseType(200, Type = typeof(string))]
+        [ProducesResponseType(400)]
+        public IActionResult Post([FromBody] LoginRequest request)
+        {
+            Validate(request);
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem();
+            }
+            string token = Process(request);
+            return Ok(token);
+        }
+        private void Validate(LoginRequest request)
+        {
+            if (string.IsNullOrEmpty(request.CompanyCode))
+            {
+                ModelState.AddModelError("CompanyCode", "Company Code cannot be blank");
+            }
+            else if (request.CompanyCode != "BBK" && request.CompanyCode != "CMAX" && request.CompanyCode != "INVI")
+            {
+                ModelState.AddModelError("CompanyCode", "Invalid Company Code");
+            }
+
+            if (string.IsNullOrEmpty(request.Username))
+            {
+                ModelState.AddModelError("Username", "Username cannot be blank");
+            }
+
+            if (string.IsNullOrEmpty(request.Password))
+            {
+                ModelState.AddModelError("Password", "Password cannot be blank");
+            }
+
+            if (ModelState.IsValid)
+            {
+                if (request.Username != "soh1763" || request.Password != "123")
+                {
+                    ModelState.AddModelError("Username", "Invalid username or password");
+                }
+            }
+        }
+
+        private static string Process(LoginRequest request)
+        {
+            var issuer = "bbk-hrms";
+            var audience = "bbk-hrms";
+            var key = Encoding.ASCII.GetBytes("bbk-hrms-secret-key");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("Id", Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Sub, request.Username ?? ""),
+                    new Claim(JwtRegisteredClaimNames.Email, "sohil.ravjani@bbkonline.com"),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                 }),
+                Expires = DateTime.UtcNow.AddMinutes(5),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            return jwtToken;
+        }
+    }
+
+    public record LoginRequest(string? CompanyCode, string? Username, string? Password);
+}
